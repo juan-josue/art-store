eae2380d790a571553c62f530c0fde9470319fbb
juan-josue <juansemail2001@gmail.com>
2025-05-06 23:35:23 -0600

    feat: add get and delete user routes

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -3,6 +3,7 @@ from flask import Flask
 from supabase import create_client
 
 from app.routes.auth import auth_bp
+from app.routes.users import users_bp
 
 app = Flask(__name__)
 
@@ -13,3 +14,4 @@ app.supabase = create_client(db_url, db_key)
 
 # Register Routes
 app.register_blueprint(auth_bp)
+app.register_blueprint(users_bp)
