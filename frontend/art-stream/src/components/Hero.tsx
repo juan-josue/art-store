50da2d0a0ecd344d81e8bffa72407a1a225ffece
juan-josue <juansemail2001@gmail.com>
2025-05-26 22:26:26 -0400

    feat: improve hero section design

--- frontend/art-stream/src/components/Hero.tsx
+++ frontend/art-stream/src/components/Hero.tsx
@@ -45,32 +45,32 @@ export default function Hero() {
           preload="auto"
           className="w-full h-full object-cover"
         >
-          <source src="/videos/bg-video.mp4" type="video/mp4" />
+          <source src="/videos/bg-video-1.mp4" type="video/mp4" />
           Your browser does not support the video tag.
         </video>
 
         {/* ART + description + call to action btn  */}
-        <div className="absolute top-10 left-4 z-10 flex flex-col gap-4 text-white">
-          <h1 className="text-[250px] font-shrikhand font-extrabold text-amber-200">
+        <div className="absolute top-32 left-32 z-10 flex flex-col text-white">
+          <h1 className="text-[250px] leading-none font-shrikhand font-extrabold text-amber-200">
             ART
           </h1>
-          <p className="text-md font-body text-white">
+          <p className="text-lg font-body text-white">
             Curated digital artwork from emerging creators. <br />
             Upload, sell, and collect with secure instant delivery.
           </p>
-          <Button className="bg-amber-200 rounded-4xl p-8 text-foreground w-40 cursor-pointer">
+          <Button className="bg-amber-200 hover:bg-pink-300 mt-4 rounded-4xl p-4 text-foreground w-40 cursor-pointer">
             Browse Art
           </Button>
         </div>
 
         {/* STREAM foreground */}
-        <h1 className="absolute bottom-4 right-4 z-10 text-[250px] font-shrikhand font-extrabold text-amber-200">
+        <h1 className="absolute bottom-4 right-32 z-10 text-[250px] font-shrikhand font-extrabold text-amber-200">
           STREAM
         </h1>
       </div>
 
       {/* STREAM background */}
-      <h1 className="absolute bottom-4 right-4 z-5 text-[250px] font-shrikhand font-extrabold text-white">
+      <h1 className="absolute bottom-4 right-32 z-5 text-[250px] font-shrikhand font-extrabold text-white">
         STREAM
       </h1>
     </section>
