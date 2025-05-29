47fd2b2d561cb162a1e077b606c8f45c12aebb1c
juan-josue <juansemail2001@gmail.com>
2025-05-29 01:42:09 -0400

    feat: implement login form with submission handling

--- frontend/art-stream/src/components/login-form.tsx
+++ frontend/art-stream/src/components/login-form.tsx
@@ -1,14 +1,42 @@
-import { cn } from "@/lib/utils";
+"use client";
+
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 
-export function LoginForm({
-  className,
-  ...props
-}: React.ComponentPropsWithoutRef<"form">) {
+export function LoginForm() {
+  async function handleSubmit(e: React.FormEvent<HTMLFormElement>) {
+    // prevent default submit behaviour (refresh page)
+    e.preventDefault();
+
+    // Extract form data
+    const formData = new FormData(e.currentTarget);
+    const email = formData.get("email");
+    const password = formData.get("password");
+
+    // Send login request to server
+    const response = await fetch("http://127.0.0.1:5000/auth/login", {
+      method: "POST",
+      credentials: "include",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ email: email, password: password }),
+    });
+
+    if (response.ok) {
+      // Handle successful login
+      const data = await response.json();
+      console.log(data.message);
+    } else {
+      // Handle unsuccessful login
+      const error = await response.json();
+      console.error("Login failed:", error);
+    }
+  }
+
   return (
-    <form className={cn("flex flex-col gap-6", className)} {...props}>
+    <form onSubmit={handleSubmit} className="flex flex-col gap-6">
       <div className="flex flex-col items-center gap-2 text-center">
         <h1 className="text-2xl font-bold">Login to your account</h1>
         <p className="text-balance text-sm text-muted-foreground">
@@ -18,7 +46,13 @@ export function LoginForm({
       <div className="grid gap-6">
         <div className="grid gap-2">
           <Label htmlFor="email">Email</Label>
-          <Input id="email" type="email" placeholder="m@example.com" required />
+          <Input
+            id="email"
+            name="email"
+            type="email"
+            placeholder="m@example.com"
+            required
+          />
         </div>
         <div className="grid gap-2">
           <div className="flex items-center">
@@ -30,7 +64,7 @@ export function LoginForm({
               Forgot your password?
             </a>
           </div>
-          <Input id="password" type="password" required />
+          <Input id="password" name="password" type="password" required />
         </div>
         <Button type="submit" className="w-full">
           Login
