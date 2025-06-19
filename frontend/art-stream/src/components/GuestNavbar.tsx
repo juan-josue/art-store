6116374e46e51adc590a655cf5e629ca5ad7712d
juan-josue <juansemail2001@gmail.com>
2025-06-19 00:10:17 -0400

    feat: update guest navbar element sizes

--- frontend/art-stream/src/components/GuestNavbar.tsx
+++ frontend/art-stream/src/components/GuestNavbar.tsx
@@ -1,5 +1,6 @@
 import { Search } from "lucide-react";
 import { LogIn } from "lucide-react";
+import { Aperture } from 'lucide-react';
 
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
@@ -8,15 +9,13 @@ export default function GuestNavbar() {
   return (
     <div className="border-b-1 py-4">
       <div className="flex flex-col w-full max-w-7xl mx-auto items-center p-4 justify-between">
-        <div className="flex w-full gap-8 font-body text-xl">
-          <h1 className="font-shrikhand font-foreground text-4xl font-extrabold text-nowrap">
-            ART STREAM
-          </h1>
-          <Input className="w-full" placeholder="What are you looking for?" />
-          <Button variant={"secondary"}>
+        <div className="flex w-full gap-8 font-body text-xl items-center">
+          <Aperture className="w-16 h-16" />
+          <Input className="w-full h-16" placeholder="What are you looking for?" />
+          <Button variant={"secondary"} className="h-16 w-32">
             <Search /> Search
           </Button>
-          <Button>
+          <Button className="h-16 w-32">
             <LogIn /> Login
           </Button>
         </div>
